afc7f682815aed3a3377a184793bccaa7a414bfe
portab76 <163771393+portab76@users.noreply.github.com>
2025-03-11 21:47:54 +0100

    Add files via upload

--- db.php
+++ db.php
@@ -2,8 +2,8 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 $servername = "SERVER_NAME";
-$username = "MYSQL_USERNAME";
-$password = "MYSQL_PASSWOD";
-$dbname = "MYSQL_DBNAME";
+$username = "USERNAME";
+$password = "PASSWORD";
+$dbname = "DBNAME";
 $conn = new mysqli($servername, $username, $password, $dbname);
 ?>
